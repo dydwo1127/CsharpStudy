1baed9ee9229b5999ba62c5486d297c13d3d246b
Song <dydwo1127@gmail.com>
2016-07-13 12:54:16 +0900

    no message

---
+++ Study2/Study2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study2
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        public Book(string title)
+        {
+            this.title = title;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Necronomicon");
+        }
+    }
+}
